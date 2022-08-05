6f09b53725d2d519e50e7e3e07f77be2b967b972
Lykasha <lykashalysongtseng@gmail.com>
2022-08-05 11:01:24 -0500

    finished models config

--- src/main/java/com/project2/models/Category.java
+++ src/main/java/com/project2/models/Category.java
@@ -1,5 +1,65 @@
 package com.project2.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Category")
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="StoreId")
+	private int id;
+	
+	@Column(name="StoreName")
+	private String name;
+	
+	@Column(name="Description")
+	private String description;
+
+	public Category() {
+		super();
+	}
+
+	public Category(int id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+	
 }
--- src/main/java/com/project2/models/Store.java
+++ src/main/java/com/project2/models/Store.java
@@ -1,5 +1,65 @@
 package com.project2.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Store")
 public class Store {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="StoreId")
+	private int id;
+	
+	@Column(name="StoreName")
+	private String name;
+	
+	@Column(name="Location")
+	private String location;
+
+	public Store() {
+		super();
+	}
+
+	public Store(int id, String name, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public String toString() {
+		return "Store [id=" + id + ", name=" + name + ", location=" + location + "]";
+	}
+	
 }
